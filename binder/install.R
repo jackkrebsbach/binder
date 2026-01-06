0a87edabd43ca93203ecd0386c6ae34187a9fcf5
Jack Krebsbach <krebsbachjackson@gmail.com>
2026-01-06 14:53:43 -0500

    update wendy & install

--- binder/install.R
+++ binder/install.R
@@ -1,9 +1,26 @@
+options(
+  torch.download_non_interactive = TRUE,
+  torch.download_quiet = TRUE
+)
+
 install.packages(c(
   "IRkernel",
   "tidyverse",
   "data.table",
-  "ggplot2"
+  "ggplot2",
+  "remotes",
+  "symengine",
+  "numbers",
+  "trust",
+  "torch",
+  "deSolve",
 ), repos = "https://cloud.r-project.org")
+
+
+torch::install_torch()
+
+library(remotes)
+install_github("jackkrebsbach/rwendy")
 
 IRkernel::installspec(user = FALSE)
 
